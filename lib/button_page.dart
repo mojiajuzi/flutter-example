ae75dead9c27e9248c9d4170b6106dd94e5ea2ad
gru <1065407867@qq.com>
2018-03-22 09:23:35 +0800

    raised button

---
+++ lib/button_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => new _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('Button demo'),
+      ),
+      body: Center(
+        child: Row(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Hello'),
+              textColor: Colors.orange,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Color'),
+              textTheme: ButtonTextTheme.accent,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Hello'),
+              color: Colors.orange,
+              elevation: 2.0,
+              highlightElevation: 8.0,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Hello'),
+              padding: const EdgeInsets.only(
+                left: 10.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'tabbar_page.dart';
+import 'button_page.dart';
 
 void main() => runApp(new Demo());
 
@@ -9,6 +9,6 @@ class Demo extends StatelessWidget {
     return MaterialApp(
         title: 'Kodeversitas',
         theme: ThemeData(primarySwatch: Colors.lightBlue),
-        home: MyTabBar());
+        home: MyButton());
   }
 }
